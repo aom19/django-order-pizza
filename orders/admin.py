dab35faf4df3768c4c012f1701a9741190329436
aom19 <39848284+aom19@users.noreply.github.com>
2023-02-02 20:17:11 +0200

    set orders and user in admin panel

--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Order
 
 # Register your models here.
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display=['id','flavour','size','order_status','size' ,'created_at']
+    list_filter=['order_status' , 'size','created_at']
--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,38 @@
+
+
 from django.db import models
+from django.contrib.auth import get_user_model
+from django.utils import timezone
+
+
+User=get_user_model()
 
 # Create your models here.
+class Order(models.Model):
+    PIZZA_SIZES=(
+        ('S','Small'),
+        ('M','Medium'),
+        ('L','Large'),
+        ('XL','Extra Large')
+
+    )
+
+    ORDER_STATUSES=(
+        ('P','Pending'),
+        ('C','Completed'),
+        ('D','Delivered'),
+        ('R','Rejected')
+
+    )
+
+    order_status=models.CharField(max_length=25,choices=ORDER_STATUSES,default=ORDER_STATUSES[0][0])
+    size=models.CharField(max_length=25,choices=PIZZA_SIZES,default=PIZZA_SIZES[0][0])
+    quantity=models.IntegerField()
+    flavour=models.CharField(max_length=40)
+    customer=models.ForeignKey(User,on_delete=models.CASCADE,null=False)
+    placed_at=models.DateTimeField(auto_now_add=True)
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"<Order {self.flavour} by {self.customer}>"
