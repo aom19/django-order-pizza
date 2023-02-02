b0d264bdd1c692171f88f26a1d94c9124914554c
aom19 <39848284+aom19@users.noreply.github.com>
2023-02-02 21:43:49 +0200

    set authentication

---
+++ authentication/serializers.py
@@ -0,0 +1,29 @@
+from .models import User
+from rest_framework import serializers
+from phonenumber_field.serializerfields import PhoneNumberField
+
+class UserCreationSerializer(serializers.ModelSerializer):
+    username = serializers.CharField(max_length=255,required=True)
+    email = serializers.EmailField(max_length=90,required=True)
+    phone_number = PhoneNumberField(allow_null=False,allow_blank=False )
+    password = serializers.CharField(min_length=8,write_only=True,required=True)
+
+    class Meta:
+        model = User
+        fields = ['username','email','phone_number','password']
+
+
+    def validate(self,attrs):
+        username_exists = User.objects.filter(username=attrs['username']).exists()
+        email_exists = User.objects.filter(email=attrs['email']).exists()
+        phonenumber_exists = User.objects.filter(phone_number=attrs['phone_number']).exists()
+
+        if username_exists:
+            raise serializers.ValidationError(detail={'username':'Username already exists'})
+        if email_exists:
+            raise serializers.ValidationError(detail={'email':'Email already exists'})
+        if phonenumber_exists:
+            raise serializers.ValidationError(detail={'phone_number':'Phone number already exists'})
+        return super().validate(attrs)
+
+
--- authentication/urls.py
+++ authentication/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import HelloAuthView
+from .views import HelloAuthView, UserCreationView
 
 urlpatterns = [
     path('', HelloAuthView.as_view(), name='hello-auth'),
+    path('signup/', UserCreationView.as_view(), name='sign_up'),
 ]
