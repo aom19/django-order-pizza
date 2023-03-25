f06513dcfef65de9d4e506997b9d18623c27daa6
aom19 <39848284+aom19@users.noreply.github.com>
2023-03-25 22:23:24 +0200

    set the nginx :

--- pizza/settings.py
+++ pizza/settings.py
@@ -26,7 +26,7 @@ from datetime import timedelta
 SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
@@ -177,6 +177,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
